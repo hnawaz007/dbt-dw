1d9f0a01509ae0c2116b9590fb77f52d1a1a13a8
Haq Nawaz <66388243+hnawaz007@users.noreply.github.com>
2023-07-25 20:09:33 -0400

    Create stg_salesorderheader.sql

---
+++ sql/stg_salesorderheader.sql
@@ -0,0 +1,36 @@
+with source as (
+
+    select * from {{ source('src_postgres', 'salesorderheader') }}
+),
+renamed as (
+
+    select 
+        status,
+        onlineorderflag,
+        taxamt,
+        purchaseordernumber,
+        "Comment", --First letter is uppercase therefore in double quoutes
+        duedate::timestamp AS duedate,
+        freight,
+        rowguid,
+        shipdate::timestamp AS shipdate,
+        subtotal,
+        totaldue,
+        orderdate::timestamp AS orderdate,
+        customerid,
+        territoryid,
+        creditcardid,
+        modifieddate,
+        salesorderid,
+        shipmethodid,
+        salespersonid,
+        currencyrateid,
+        revisionnumber,
+        billtoaddressid,
+        shiptoaddressid,
+        salesordernumber,
+        creditcardapprovalcode
+    from source
+)
+
+select * from renamed
